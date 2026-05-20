91a54b5641d7998554367fbfd9813641ed45262e
asey0u <117942915+asey0u@users.noreply.github.com>
2026-05-21 01:06:49 +0700

    Collections dev (#11)
    * initial collections commit
    
    * moved CollectionType to another .go file
    
    * architecture changed to core and adapters above it
    
    * implementation of new architecture & scripts for database running & docker files for containers
    
    * added description field to movie and changed movie's fields Genre, Actors, Directors from strings to structs & changed convertion logic due to moving string->struct in Movie struct & changed proto files by adding new Movie fields & cleaned migrations
    
    * changed signature of MovieRepository interface to more readable, List(...) -> ListMovies(...)
    
    * changed one of WatchlistRepository's signatures to more readable, GetWatchlist(...) -> GetUserWatchlist(...) & regenerated proto files with changed signatures
    
    * added proto message MovieSnapshot for showing less metadata on requests
    
    * removed metadata fields from movie and removed MovieSnapshot because it became a duplicate of Movie
    
    * renamed collection-service to movie-service
    
    * took out environment variables into .env file & added syncer for fetching films from open api service & took out proto files from movie-service, now they will lie in shared directory
    
    * added unit tests
    
    * rebased to main and fixed all imports and mod issues
    
    ---------
    
    Co-authored-by: weolence <weolence@MacBook-Pro-weolence.local>

--- movie-service/internal/adapters/redis/movie_cache.go
+++ movie-service/internal/adapters/redis/movie_cache.go
@@ -7,10 +7,9 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"time"
-
 	"github.com/BigMoneyBigSuccess/cineMate/movie-service/internal/core/domain"
 	"github.com/BigMoneyBigSuccess/cineMate/movie-service/internal/core/ports"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/redis/go-redis/v9"
