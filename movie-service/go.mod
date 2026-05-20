1a31122575caaf95ec7181c31a00911a0cef53b3
fedor <tron890.varlakov@yandex.ru>
2026-05-20 23:08:08 +0700

    rebased to main and fixed all imports and mod issues

--- movie-service/go.mod
+++ movie-service/go.mod
@@ -10,7 +10,6 @@ require (
 	google.golang.org/grpc v1.79.3
 	google.golang.org/protobuf v1.36.11
 	gopkg.in/yaml.v3 v3.0.1
-	proto v0.0.0
 )
 
 replace github.com/BigMoneyBigSuccess/cineMate/proto => ../proto
--- movie-service/internal/adapters/redis/movie_cache.go
+++ movie-service/internal/adapters/redis/movie_cache.go
@@ -7,8 +7,8 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"movie_service/internal/core/domain"
-	"movie_service/internal/core/ports"
+	"github.com/BigMoneyBigSuccess/cineMate/movie-service/internal/core/domain"
+	"github.com/BigMoneyBigSuccess/cineMate/movie-service/internal/core/ports"
 	"time"
 
 	"github.com/google/uuid"
